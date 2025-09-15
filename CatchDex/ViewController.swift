f1b2144125f77f9fff1856d6bd4a40d63efce191
NandaPerovano <nandaperovanorm@gmail.com>
2025-09-15 11:20:26 -0300

    Primeiro commit com projeto inicial

---
+++ CatchDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchDex
+//
+//  Created by Fernanda Perovano on 15/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ CatchDexTests/CatchDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchDexTests.swift
+//  CatchDexTests
+//
+//  Created by Fernanda Perovano on 15/09/25.
+//
+
+import Testing
+@testable import CatchDex
+
+struct CatchDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
